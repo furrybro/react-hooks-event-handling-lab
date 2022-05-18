fbc97ab3fef0027e2e5c4c7b410c589043ab4a0b
Jeena Park <jeenapark15@gmail.com>
2022-05-18 18:38:15 -0400

    commit tree 54d0341694fad5eadeb499088843245fbc2d61ff

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function focusButton() {
+        console.log("Good!");
+    }
+
+    function blurButton() {
+        console.log("Hey! Eyes on me!");
+    }
+
+    return <button onFocus={focusButton} onBlur={blurButton}>Eyes on me</button>
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+
+    function inputPassword(e) {
+        console.log("Entering password...");
+    }
+
+    return (
+        <input type="password" onChange={inputPassword}></input>
+    );
+}
+
+
+export default Keypad;
